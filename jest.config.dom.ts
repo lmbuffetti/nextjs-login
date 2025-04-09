eefc0495abb5e11fe98ff9cb60b6de1f1fabcdc3
mbuffetti <mattia.buffetti@gmail.com>
2025-04-09 02:09:06 +0100

    test run cypress

--- jest.config.dom.ts
+++ jest.config.dom.ts
@@ -20,6 +20,9 @@ const customJestConfig = {
   testEnvironmentOptions: {
     url: 'http://localhost:3000',
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
--- jest.config.node.ts
+++ jest.config.node.ts
@@ -20,6 +20,9 @@ const customJestConfigNode = {
   testEnvironmentOptions: {
     url: 'http://localhost:3000',
   },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
